2d63e470d13c427d64f4af11691bb59162952f6c
dbarwick10 <dbarwick10@users.noreply.github.com>
2024-10-27 16:35:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -42,6 +42,14 @@ app.get('/liveclientdata/allgamedata', async (req, res) => {
     }
 });
 
+// Middleware to serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Example route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Proxy server running on http://localhost:${PORT}`);
