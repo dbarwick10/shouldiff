9297b93ac017e8d7bb7a96890b9e4179bc55dfd4
d.barwick <dbarwick10@users.noreply.github.com>
2024-12-14 21:32:47 -0500

    s

--- client/js/index.js
+++ client/js/index.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 loading.style.display = 'flex';
 
                 // Fetch data from server
-                const response = await fetch('http://18.222.118.84:3000/api/stats', {
+                const response = await fetch('https://18.222.118.84:3000/api/stats', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
